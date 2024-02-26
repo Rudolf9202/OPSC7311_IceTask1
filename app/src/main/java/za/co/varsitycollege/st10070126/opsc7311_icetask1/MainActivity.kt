8e156fc72ef9215e026ddece5ee1165e4b370ccd
Rudolf9202 <st10070126@vcconnect.edu.za>
2024-02-26 20:21:33 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10070126/opsc7311_icetask1/MainActivity.kt
@@ -0,0 +1,11 @@
+package za.co.varsitycollege.st10070126.opsc7311_icetask1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
